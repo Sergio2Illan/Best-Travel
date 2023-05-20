49ab5e637706e9377185f8be398dee61755e26da
Sergio Illán <illan2sergio@gmail.com>
2023-05-20 20:19:07 +0200

    add new entities and repositories

---
+++ best_travel/src/main/java/com/initSeven/best_travel/domain/repositories/FlyRepository.java
@@ -0,0 +1,9 @@
+package com.initSeven.best_travel.domain.repositories;
+
+import com.initSeven.best_travel.domain.entities.FlyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlyRepository extends JpaRepository<FlyEntity, Long>{
+
+
+}
